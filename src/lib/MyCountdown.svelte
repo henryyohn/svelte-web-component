5a8d8db07df5a4eb319a12c7e03e07fe5bb60f8a
henry <henry@goddeMacBook-Pro.local>
2023-01-19 13:51:13 +0800

    finish test

---
+++ src/lib/MyCountdown.svelte
@@ -0,0 +1,39 @@
+<!--Add this line to your web component-->
+<svelte:options tag="my-counter" />
+
+<script>
+  let count = 0;
+  let timer = null;
+  let status = "";
+  const countdown = () => {
+    timer = setTimeout(() => {
+      count = count + 1;
+      countdown();
+    }, 1000);
+  };
+  const stop = () => {
+    if (timer) {
+      timer && clearTimeout(timer);
+      timer = null;
+      status = "stop";
+    } else {
+      countdown();
+      status = "running";
+    }
+  };
+</script>
+
+<p>{status}</p>
+<button on:click={stop}>
+  count seconds: {count}
+</button>
+
+<style>
+  button {
+    border: 1px solid #000;
+    background-color: #fff;
+    padding: 1.5em;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import './app.css'
+// import App from './App.svelte'
+export * from './lib/MyCountdown.svelte'
+export * from './lib/Clock.svelte'
+export * from './lib/Button.svelte'
+export * from './lib/Card.svelte'
+
+
+// const app = new App({
+//   target: document.getElementById('app'),
+// })
+
+// export default app
